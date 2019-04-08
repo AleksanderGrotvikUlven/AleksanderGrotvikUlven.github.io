a045162788e107fb849afea9b1d222ec65a47be4
Aleksander Grøtvik Ulven <aleks.ulven@gmail.com>
2019-04-08 13:05:33 +0200

    Første utkast
    Je er ferdig med nesten alt, mangler en spørring som sorterer etter pris Høy til Lav. Resten skal mer eller mindre være ferdig.

--- Prosjekt_vår_2019/script/script.js
+++ Prosjekt_vår_2019/script/script.js
@@ -4,3 +4,27 @@ function visMeny() {
   var bars = document.getElementById("bars");
   bars.classList.toggle("fa-times");
 }
+function visButton1() {
+  var sorteringsknapp1 = document.getElementById("sorteringsknapp1");
+  var sorteringsknapp2 = document.getElementById("sorteringsknapp2");
+  var sorteringsknapp3 = document.getElementById("sorteringsknapp3");
+  sorteringsknapp1.classList.toggle("hidden");
+  sorteringsknapp2.classList.add("hidden");
+  sorteringsknapp3.classList.add("hidden");
+}
+function visButton2() {
+  var sorteringsknapp1 = document.getElementById("sorteringsknapp1");
+  var sorteringsknapp2 = document.getElementById("sorteringsknapp2");
+  var sorteringsknapp3 = document.getElementById("sorteringsknapp3");
+  sorteringsknapp1.classList.add("hidden");
+  sorteringsknapp2.classList.toggle("hidden");
+  sorteringsknapp3.classList.add("hidden");
+}
+function visButton3() {
+  var sorteringsknapp1 = document.getElementById("sorteringsknapp1");
+  var sorteringsknapp2 = document.getElementById("sorteringsknapp2");
+  var sorteringsknapp3 = document.getElementById("sorteringsknapp3");
+  sorteringsknapp1.classList.add("hidden");
+  sorteringsknapp2.classList.add("hidden");
+  sorteringsknapp3.classList.toggle("hidden");
+}
