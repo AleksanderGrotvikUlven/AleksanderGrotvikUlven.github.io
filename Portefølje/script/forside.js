8ced46bd4131617abd82fccf30667c7732046f10
Aleksander Grøtvik Ulven <aleks.ulven@gmail.com>
2019-05-13 13:06:03 +0200

    Portefølje start

---
+++ Portefølje/script/forside.js
@@ -0,0 +1,6 @@
+function visMeny() {
+  var dropdown = document.getElementById("dropdown");
+  dropdown.classList.toggle("hidden");
+  var bars = document.getElementById("bars");
+  bars.classList.toggle("fa-times");
+}
